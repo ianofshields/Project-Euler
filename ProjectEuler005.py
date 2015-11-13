446de791aecc8d5bd291f1d464135846c2df5b1a
ianofshields <ianmcglasham@gmail.com>
2015-11-13 12:32:28 +0000

    Update ProjectEuler005.py

--- ProjectEuler005.py
+++ ProjectEuler005.py
@@ -1,4 +1,4 @@
-# Project Euler problem 005.
+# Project Euler - problem 005 - SMALLEST MULTIPLE
 #
 # 2520 is the smallest number that can be divided by each of the
 # numbers from 1 to 10 without any remainder.
