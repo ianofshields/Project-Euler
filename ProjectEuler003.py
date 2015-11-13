dc165b957462dd5ac3b4d131ccb238b9855adc99
ianofshields <ianmcglasham@gmail.com>
2015-11-13 10:47:33 +0000

    Update ProjectEuler003.py

--- ProjectEuler003.py
+++ ProjectEuler003.py
@@ -1,13 +1,17 @@
-# python
+# Project Euler problem 001.
+#
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
 
-a = 600851475143
-i = 2;
+target = 600851475143
+primetest = 2;
 
-while (a > i):
-	if (a%i == 0):
-		a /= i
-		i = 2
+while (target > primetest):
+	if (target % primetest == 0):
+		target /= primetest
+		primetest = 2
 	else:
-		i += 1
+		primetest += 1
 
-print i
+print "The largest prime factor of the number 60085147513 is: ", primetest
